4d8c85127da30ee1cdf81855e9d89ac5229ffa90
Brian R. Becker <brbecker@gmail.com>
2021-08-28 03:39:07 +0000

    Step 4 complete

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,7 +1,14 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from collections.abc import Iterable
 
+
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
+    @abstractmethod
+    def is_due(self):
+        pass
+
+
+class DeadlinedReminder(Iterable, ABC):
     @abstractmethod
     def is_due(self):
         pass
