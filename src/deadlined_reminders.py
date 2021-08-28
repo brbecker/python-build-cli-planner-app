54fbef7e73b8d4d0fd1bfde07fab6fbe1a520bc1
Brian R. Becker <brbecker@gmail.com>
2021-08-28 03:55:31 +0000

    Step 5 complete

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,6 +1,8 @@
 from abc import ABC, ABCMeta, abstractmethod
 from collections.abc import Iterable
 
+from dateutil.parser import parse
+
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
     @abstractmethod
@@ -12,3 +14,9 @@ class DeadlinedReminder(Iterable, ABC):
     @abstractmethod
     def is_due(self):
         pass
+
+
+class DateReminder(DeadlinedReminder):
+    def __init__(self, text: str, date: str) -> None:
+        self.date = parse(date, dayfirst=True)
+        self.text = text
