abadee438bd7062cf252b0e343e806bebb5ae095
Brian R. Becker <brbecker@gmail.com>
2021-08-30 06:25:15 +0000

    Step 12 complete

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from src.database import add_reminder, list_reminders
-from src.deadlined_reminders import DateReminder
+from src.deadlined_reminders import DateReminder, DeadlinedReminder
 from src.external_reminders import EveningReminder
+from src.reminder import PoliteReminder
+
+
+DeadlinedReminder.register(PoliteReminder)
+
 
 def handle_input():
     choice = input("Choice: ")
@@ -15,12 +20,13 @@ def handle_input():
         reminder = input("What would you like to be reminded about?: ")
         date = input("When is that due?: ")
 
-        add_reminder(reminder, date, EveningReminder)
+        add_reminder(reminder, date, PoliteReminder)
         list_reminders()
     else:
         print("Invalid menu option")
 
     return True
+
 
 def print_menu():
     print()
@@ -36,10 +42,12 @@ def print_menu():
     print('2) Add a reminder')
     print('3) Exit')
 
+
 def main():
     print_menu()
     while handle_input():
         print_menu()
+
 
 if __name__ == '__main__':
     main()
